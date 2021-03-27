7a917c2779c9a7dbce577a47ba8cb575bb581e31
Ata-92 <atakal92@gmail.com>
2021-03-27 16:27:50 +0300

    Add reverser function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+function reverser(text) {
+  while (text.trim().split(" ").length < 2) {
+    text = prompt(`Your text => ${text} <= is a single word.\nPlease enter at least two words :`);
+  }
+  let reversedText = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    reversedText += text[i];
+  }
+  alert(`Your reversed text is : ${reversedText}`);
+}
