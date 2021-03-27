194c2abbe2561cb7b08678b287d00d58a1502b74
Ata-92 <atakal92@gmail.com>
2021-03-27 17:03:05 +0300

    Add tests

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function reverser(text) {
+function reverseWords(text) {
   while (text.trim().split(" ").length < 2) {
     text = prompt(`Your text => ${text} <= is a single word.\nPlease enter at least two words :`);
   }
@@ -10,4 +10,41 @@ function reverser(text) {
 }
 
 const myText = prompt("Please enter a text of at least two words to reverse : ");
-console.log(reverser(myText));
+console.log(reverseWords(myText));
+
+// Tests
+
+// let desc = 'Don\'t reverse single word';
+// let input = 'Clarusway'.split('');
+// reverseWords(input);
+// let actual = input.join('');
+// let expected = 'Clarusway';
+// assertEqual(actual, expected, desc);
+
+// desc = 'Inspring Quote #1';
+// input = 'days. big make things Little'.split('');
+// reverseWords(input);
+// actual = input.join('');
+// expected = 'Little things make big days.';
+// assertEqual(actual, expected, desc);
+
+// desc = 'Inspring Quote #2';
+// input = "impossible. mean not does hard but hard, be to going It’s".split('');
+// reverseWords(input);
+// actual = input.join('');
+// expected = "It’s going to be hard, but hard does not mean impossible.";
+// assertEqual(actual, expected, desc);
+
+// desc = 'Inspring Quote #3';
+// input = "done. you’re when Stop tired. you’re when stop Don’t".split('');
+// reverseWords(input);
+// actual = input.join('');
+// expected = "Don’t stop when you’re tired. Stop when you’re done.";
+// assertEqual(actual, expected, desc);
+// function assertEqual(a, b, desc) {
+// if (a === b) {
+//     console.log(`${desc} ... PASS`);
+// }else {
+//     console.log(`${desc} ... FAIL: ${a} ≠ ${b}`);
+// 	}
+// }
