d6ff821d3abb6c317dc58bfe438d8ac27c83a1cd
Ata-92 <atakal92@gmail.com>
2021-03-28 23:28:19 +0300

    Add a new solution without reassigning input

--- app.js
+++ app.js
@@ -14,9 +14,7 @@
 // const myText = prompt("Please enter a text of at least two words to reverse : ");
 // console.log(reverser(myText));
 
-
 // Solutions For Tests
-
 
 // String Solution
 
@@ -39,13 +37,8 @@
 // 1 Reverse Method
 
 // function reverseWords(textArray) {
-//   if (textArray.join("").trim().split(" ").length < 2) {
-//     input = textArray;
-//   } else {
-//     input = textArray.join("").split(" ").reverse().join(" ").split("");
-//   }
+//   input = textArray.join("").split(" ").reverse().join(" ").split("");
 // }
-
 
 // 2 Push Method
 
@@ -61,7 +54,6 @@
 //     input = reversedTextArray.join(" ").split("");
 //   }
 // }
-
 
 // 3 Replacing
 
@@ -79,40 +71,60 @@ function reverseWords(textArray) {
   }
 }
 
+// Solution Without Reassigning Input
+
+// function reverseWords(message) {
+//   let result = message;
+//   message.reverse();
+//   let xthWord = "";
+//   let counter = 0;
+//   for (let i = 0; i < message.length + 1; i++) {
+//     if (message[i] === " " || i === message.length) {
+//       for (let j = 0; j < xthWord.length; j++) {
+//         result[counter + j] = xthWord[xthWord.length - j - 1];
+//       }
+//       counter = i + 1;
+//       xthWord = "";
+//       continue;
+//     }
+//     xthWord += message[i];
+//   }
+//   return result;
+// }
 
 // Tests
 
-let desc = 'Don\'t reverse single word';
-let input = 'Clarusway'.split('');
+let desc = "Don't reverse single word";
+let input = "Clarusway".split("");
 reverseWords(input);
-let actual = input.join('');
-let expected = 'Clarusway';
+let actual = input.join("");
+let expected = "Clarusway";
 assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #1';
-input = 'days. big make things Little'.split('');
+desc = "Inspring Quote #1";
+input = "days. big make things Little".split("");
 reverseWords(input);
-actual = input.join('');
-expected = 'Little things make big days.';
+actual = input.join("");
+expected = "Little things make big days.";
 assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #2';
-input = "impossible. mean not does hard but hard, be to going It’s".split('');
+desc = "Inspring Quote #2";
+input = "impossible. mean not does hard but hard, be to going It’s".split("");
 reverseWords(input);
-actual = input.join('');
+actual = input.join("");
 expected = "It’s going to be hard, but hard does not mean impossible.";
 assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #3';
-input = "done. you’re when Stop tired. you’re when stop Don’t".split('');
+desc = "Inspring Quote #3";
+input = "done. you’re when Stop tired. you’re when stop Don’t".split("");
 reverseWords(input);
-actual = input.join('');
+actual = input.join("");
 expected = "Don’t stop when you’re tired. Stop when you’re done.";
 assertEqual(actual, expected, desc);
 function assertEqual(a, b, desc) {
-if (a === b) {
+  if (a === b) {
     console.log(`${desc} ... PASS`);
-}else {
+  } else {
     console.log(`${desc} ... FAIL: ${a} ≠ ${b}`);
-	}
+  }
 }
